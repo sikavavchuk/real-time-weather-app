5c2d3d0e1c549b7530bd1b1e713b3f4b711b28a4
Viktoriia <savchukv2006@gmail.com>
2026-03-26 23:06:48 +0100

    Connect UI with code

--- RealTimeWeather/ViewController.swift
+++ RealTimeWeather/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        
+    }
+    
 }
 
