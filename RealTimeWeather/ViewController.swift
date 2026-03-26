1e93c7ee9c33465ff2e65fa7d04d44cd0c973e18
Viktoriia <savchukv2006@gmail.com>
2026-03-26 11:42:52 +0100

    Initial Commit

---
+++ RealTimeWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTimeWeather
+//
+//  Created by Viktoriia Savchuk  on 26/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
