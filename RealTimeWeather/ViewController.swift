191ced9cc246ec85183055017889448de1e46f11
Viktoriia <savchukv2006@gmail.com>
2026-03-26 23:13:18 +0100

    Add methods in ViewController Class

--- RealTimeWeather/ViewController.swift
+++ RealTimeWeather/ViewController.swift
@@ -26,5 +26,30 @@ class ViewController: UIViewController {
         
     }
     
+    //hide keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        textField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool{
+        if textField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Please enter a city name"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let city = searchTextField.text {
+            //
+        }
+    }
+    
+    
+    
 }
 
