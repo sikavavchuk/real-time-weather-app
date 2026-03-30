547ec75f4010bca39860a8f618e0e3e0ca50bcad
Viktoriia <savchukv2006@gmail.com>
2026-03-30 11:36:52 +0200

    Implement current locationof user

--- RealTimeWeather/Controller/WeatherViewController.swift
+++ RealTimeWeather/Controller/WeatherViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
     
@@ -16,14 +17,19 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var weatherDescriptionLabel: UILabel!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
         weatherManager.delegate = self
         searchTextField.delegate = self
     }
-
+    
 }
 
 //MARK: - UITextFieldDelegate
@@ -63,6 +69,9 @@ extension WeatherViewController: UITextFieldDelegate {
     
 }
 
+//MARK: - WeatherManagerDelegate
+
+
 extension WeatherViewController: WeatherManagerDelegate {
     
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
@@ -73,6 +82,23 @@ extension WeatherViewController: WeatherManagerDelegate {
     }
     
     func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+protocol LocationManagerDelegate {
+    func didUpdateLocation(_ locationManager: CLLocationManager, location: CLLocation)
+    func didFailWithError(error: Error)
+}
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    @objc func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Got location data!")
+    }
+    
+    @objc func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
 }
